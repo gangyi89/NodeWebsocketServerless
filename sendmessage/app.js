7e69af3781fcb4b1f17311a5f68b9a637929cb5c
gangyi <gangyi@eatsyapp.co>
2020-06-03 04:35:41 +0800

    include subscribe

--- sendmessage/app.js
+++ sendmessage/app.js
@@ -1,46 +1,88 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
-const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
+const ddb = new AWS.DynamoDB.DocumentClient({
+  apiVersion: "2012-08-10",
+  region: process.env.AWS_REGION,
+});
 
 const { TABLE_NAME } = process.env;
 
-exports.handler = async event => {
+exports.handler = async (event) => {
   let connectionData;
-  
+  console.log("receive send message request");
+
+  const postAction = JSON.parse(event.body).action;
+  const postData = JSON.parse(event.body).data;
+
+  if (postAction === "subscribe") {
+    const updateParams = {
+      TableName: TABLE_NAME,
+      Key: { connectionId: event.requestContext.connectionId },
+      UpdateExpression: "set #channel = :channel",
+      ExpressionAttributeNames: { "#channel": "channel" },
+      ExpressionAttributeValues: { ":channel": postData },
+      ReturnValues: "UPDATED_NEW",
+    };
+
+    try {
+      await ddb.update(updateParams).promise();
+    } catch (e) {
+      console.log("logging update item error message");
+      console.log(e.stack);
+      return { statusCode: 500, body: e.stack };
+    }
+  }
+
   try {
-    connectionData = await ddb.scan({ TableName: TABLE_NAME, ProjectionExpression: 'connectionId' }).promise();
+    //TO-DO scan is not optimised, but it gets the job one
+    const params = {
+      TableName: TABLE_NAME,
+      ProjectionExpression: "connectionId",
+      FilterExpression: "#channel = :channel",
+      ExpressionAttributeNames: { "#channel": "channel" },
+      ExpressionAttributeValues: { ":channel": "general" },
+    };
+    connectionData = await ddb.scan(params).promise();
+    // connectionData = await ddb
+    //   .scan({ TableName: TABLE_NAME, ProjectionExpression: "connectionId" })
+    //   .promise();
   } catch (e) {
+    console.log("logging db query error message");
+    console.log(e.stack);
     return { statusCode: 500, body: e.stack };
   }
-  
+
   const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-    apiVersion: '2018-11-29',
-    endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
+    apiVersion: "2018-11-29",
+    endpoint:
+      event.requestContext.domainName + "/" + event.requestContext.stage,
   });
-  
-  const postData = JSON.parse(event.body).data;
-  
+
   const postCalls = connectionData.Items.map(async ({ connectionId }) => {
     try {
-      await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
+      await apigwManagementApi
+        .postToConnection({ ConnectionId: connectionId, Data: postData })
+        .promise();
     } catch (e) {
       if (e.statusCode === 410) {
         console.log(`Found stale connection, deleting ${connectionId}`);
-        await ddb.delete({ TableName: TABLE_NAME, Key: { connectionId } }).promise();
+        await ddb
+          .delete({ TableName: TABLE_NAME, Key: { connectionId } })
+          .promise();
       } else {
         throw e;
       }
     }
   });
-  
+
   try {
     await Promise.all(postCalls);
   } catch (e) {
     return { statusCode: 500, body: e.stack };
   }
 
-  return { statusCode: 200, body: 'Data sent.' };
+  return { statusCode: 200, body: "Data sent." };
 };
